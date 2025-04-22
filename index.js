5d127455ebc51da7461f2f619dfb9f6ab2624f20
JBfive <beautuesday@gmail.com>
2025-04-22 16:21:44 -0700

    still slow

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ class Typer {
             if (this.index >= this.text.length) {
                 clearInterval(this.typeTimer);
             }
-        }, 50);
+        }, 40);
     }
 
     step() {
