a6d92781b4ca23903b0441e6690c807205ae08db
JBfive <beautuesday@gmail.com>
2025-04-22 16:16:06 -0700

    increase type timer

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ class Typer {
             if (this.index >= this.text.length) {
                 clearInterval(this.typeTimer);
             }
-        }, 30);
+        }, 60);
     }
 
     step() {
